ac29d0b9c8c327e97ae977ffa24df21b07ab06d0
Samuel Catherman <samuel@stayntouch.com>
2020-12-09 10:40:47 -0800

    debug builder for synxis

--- lib/savon/builder.rb
+++ lib/savon/builder.rb
@@ -31,6 +31,8 @@ module Savon
 
       @types = convert_type_definitions_to_hash
       @used_namespaces = convert_type_namespaces_to_hash
+
+      byebug
     end
 
     def pretty
@@ -93,7 +95,11 @@ module Savon
 
     def use_namespace(path, uri)
       @internal_namespace_count ||= 0
-
+      puts "***********************************"
+      puts "use namespace path: #{path}"
+      puts "use namespace uri: #{uri}"
+      puts "***********************************"
+      byebug
       unless identifier = namespace_by_uri(uri)
         identifier = "ins#{@internal_namespace_count}"
         namespaces["xmlns:#{identifier}"] = uri
@@ -108,6 +114,12 @@ module Savon
     end
 
     def namespaces
+      puts "###################################"
+      puts "namespaces method: namespace_identifier: #{namespace_identifier}"
+      puts "namespaces method: @globals: #{@globals}"
+      puts "namespaces method: @wsdl: #{@wsdl}"
+      puts "namespaces method: env_namespace: #{env_namespace}"
+      puts "###################################"
       @namespaces ||= begin
         namespaces = SCHEMA_TYPES.dup
 
